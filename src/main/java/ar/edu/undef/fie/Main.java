1e665c7f494a231ac3efd89379eb9fcd2e757d2a
“julioleonel” <jsendra@fie.undef.edu.ar>
2023-09-10 16:34:24 -0300

    subo ejercicio resuelto

--- src/main/java/ar/edu/undef/fie/Main.java
+++ src/main/java/ar/edu/undef/fie/Main.java
@@ -2,6 +2,38 @@ package ar.edu.undef.fie;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        if (args.length == 0) {
+            System.out.println("No se proporcionaron argumentos");
+            return;
+        }
+
+        System.out.println("Ingreso los siguientes argumentos: ");
+        for (String argumento : args){
+            System.out.println(argumento + " ");
+        }
+
+        int cantidad = 0;
+        int suma = 0;
+
+        for (String argumento : args){
+            int numero = Integer.parseInt(argumento);
+                cantidad++;
+                suma += numero;
+        }
+
+        float promedio = (float) suma / cantidad;
+
+        System.out.println("El promedio de los numeros ingresados fue de: " + promedio);
+
+
+
     }
 }
+
+
+
+
+
+
+
